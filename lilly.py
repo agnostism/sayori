58476e64293a946ff4532773fd946bb8d390d028
Kaori <106315008+kaeori@users.noreply.github.com>
2022-07-26 17:50:35 +0100

    Update lilly.py

--- lilly.py
+++ lilly.py
@@ -1,7 +1,43 @@
-class Composable:
+from typing import Any, Callable
 
-	def __init__(self, callable):
+
+class Composable:
+	
+	"""
+	Implements composable functions.
+	
+	If f and g are composable functions then (f | g) is a 
+	composable function such that (f | g)(x) = g(f(x)).
+	"""
+
+	
+	def __init__(self, callable: Callable):
+		
+		"""
+		Creates a composable from a given callable.
+		"""
 
 		self.callable = callable
-		self.__call__ = lambda _, x: callable(x)
-		self.__or__ = lambda _, other: Composable(lambda x: other.callable(callable(x)))
+
+
+	
+	def __call__(self, x: Any) -> Any:
+		
+		"""
+		Calls the composable (i.e. the internal callable).
+		"""
+
+		return self.callable(x)
+
+
+	
+	def __or__(self, other: Composable) -> Composable:
+		
+		"""
+		Implements the composition/pipe operator, and returns
+		a new composable.
+		"""
+
+		return Composable(
+			lambda x: other.callable(self.callable(x))
+		)
